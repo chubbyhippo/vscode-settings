ad2cacb97056b3fb0a06f61a5e4e34b5d3acf14e
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-02-29 17:48:52 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,5 @@
 Invoke-RestMethod https://raw.githubusercontent.com/chubbyhippo/code-settings/main/install-extensions.sh | Invoke-Expression
+
+$url = "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json"
+$destinationPath = "$Home\AppData\Roaming\Code\User\settings.json"
+Invoke-WebRequest -Uri $url -OutFile $destinationPath
