a94b9c052af6d6ade94bd5eea859f74734e75948
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-02-29 17:47:06 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,1 @@
-irm https://raw.githubusercontent.com/chubbyhippo/code-settings/main/install-extensions.sh | iex
+Invoke-RestMethod https://raw.githubusercontent.com/chubbyhippo/code-settings/main/install-extensions.sh | Invoke-Expression
