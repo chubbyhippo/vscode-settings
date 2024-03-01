aedd11b7adc77562cfbda436ebba7a7b466b37ac
ch <mkmsacc@outlook.com>
2024-03-01 10:02:22 +0700

    Update extensions

--- install-extensions.sh
+++ install-extensions.sh
@@ -2,6 +2,12 @@
 
 code --install-extension vscodevim.vim
 code --install-extension k--kato.intellij-idea-keybindings
+
+# git
 code --install-extension eamodio.gitlens
+# static code analysis
 code --install-extension sonarsource.sonarlint-vscode
+# java
+code --install-extension vscjava.vscode-java-pack
+# javascript
 code --install-extension dbaeumer.vscode-eslint
