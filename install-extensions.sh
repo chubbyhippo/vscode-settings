3208a92dbe8f42d04244255350029d86078fcb69
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-03-01 09:54:05 +0700

    Update install-extensions.sh

--- install-extensions.sh
+++ install-extensions.sh
@@ -4,3 +4,4 @@ code --install-extension vscodevim.vim
 code --install-extension k--kato.intellij-idea-keybindings
 code --install-extension eamodio.gitlens
 code --install-extension sonarsource.sonarlint-vscode
+code --install-extension dbaeumer.vscode-eslint
