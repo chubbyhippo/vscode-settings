85af874e8f5ce381df5ba144ef7907f09c6bd577
ch <mkmsacc@outlook.com>
2024-03-01 10:04:46 +0700

    Add powershell extention

--- install-extensions.sh
+++ install-extensions.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# keybindings
 code --install-extension vscodevim.vim
 code --install-extension k--kato.intellij-idea-keybindings
-
 # git
 code --install-extension eamodio.gitlens
+# powershell
+code --install-extension ms-vscode.powershell
 # static code analysis
 code --install-extension sonarsource.sonarlint-vscode
 # java
