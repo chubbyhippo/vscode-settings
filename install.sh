57bf411a79e502aa63ecd3ca2a124d8dfd83498e
ch <mkmsacc@outlook.com>
2024-03-02 09:30:24 +0700

    Add installation script
    The script, install.sh, has been added to the project. It automatically determines the operating system being used, sets the appropriate destination path for settings.json file, and fetches the file using either curl or wget depending on their availability.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+destination=""
+case $(uname) in
+    "Linux")
+        destination="$HOME/.config/Code/User/settings.json"
+        ;;
+    "Darwin")
+        destination="$HOME/Library/Application\ Support/Code/User/settings.json"
+        ;;
+    "MINGW"*)
+        destination="$HOME/AppData/Roaming/Code/User/settings.json"
+        ;;
+    *)
+        echo "Unknown Operating System"
+        ;;
+esac
+
+echo "$destination"
+if [ "$(command -v curl)" ]; then
+    curl https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json -o "$destination"
+elif [ "$(command -v wget)" ]; then
+    wget https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json -P "$destination"
+fi
