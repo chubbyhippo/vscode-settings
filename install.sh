309f9176c3ef8312035b412d5b32dc0d29acad6f
ch <mkmsacc@outlook.com>
2024-03-02 16:36:53 +0700

    Update install script to include keybindings.json
    Modified the install.sh script to download both settings.json and keybindings.json. Now the script will setup both customized settings and keybindings, ensuring a personalized development environment right from the start.

--- install.sh
+++ install.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env sh
 
-# identify destination
-destination=""
+# identify settings_json_destination
+settings_json_destination=""
+keybindings_json_destination=""
 case $(uname) in
     "Linux")
-        destination="$HOME/.config/Code/User/settings.json"
+        settings_json_destination="$HOME/.config/Code/User/settings.json"
+        keybindings_json_destination="$HOME/.config/Code/User/keybindings.json"
         ;;
     "Darwin")
-        destination="$HOME/Library/Application Support/Code/User/settings.json"
+        settings_json_destination="$HOME/Library/Application Support/Code/User/settings.json"
+        keybindings_json_destination="$HOME/Library/Application Support/Code/User/keybindings.json"
         ;;
     "MINGW"*)
-        destination="$HOME/AppData/Roaming/Code/User/settings.json"
+        settings_json_destination="$HOME/AppData/Roaming/Code/User/settings.json"
+        keybindings_json_destination="$HOME/AppData/Roaming/Code/User/keybindings.json"
         ;;
     *)
         echo "Unknown Operating System"
         ;;
 esac
 # install settings.json
-echo "$destination"
+echo "$settings_json_destination"
 if [ "$(command -v curl)" ]; then
-    curl https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json -o "$destination"
+    curl https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json -o "$settings_json_destination"
+    curl https://raw.githubusercontent.com/chubbyhippo/code-settings/main/keybindings.json -o "$keybindings_json_destination"
 elif [ "$(command -v wget)" ]; then
-    wget https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json -P "$destination"
+    wget https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json -P "$settings_json_destination"
+    wget https://raw.githubusercontent.com/chubbyhippo/code-settings/main/keybindings.json -P "$keybindings_json_destination"
 fi
 
 # install extensions
