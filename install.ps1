58ae5ba3dd8a5c7f00f3188286fd579b368e83f8
ch <mkmsacc@outlook.com>
2024-03-02 16:32:29 +0700

    Update install script: include extensions
    Amended the install.ps1 script to include downloads for extensions. This adjustment guarantees all necessary extensions are automatically loaded into the user's development environment during setup, thus enhancing the programming experience.

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
+# install extensions
 Invoke-RestMethod https://raw.githubusercontent.com/chubbyhippo/code-settings/main/install-extensions.sh | Invoke-Expression
-
 # settings.json
 $url = "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json"
 $destinationPath = "$Home\AppData\Roaming\Code\User\settings.json"
