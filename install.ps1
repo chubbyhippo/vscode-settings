9cc5f89bf97d93d1b04271aca4ec3b8037568a90
ch <mkmsacc@outlook.com>
2024-03-02 16:32:01 +0700

    Add keybindings.json download to install script
    Modified the install script to download keybindings.json file from the `code-settings` repository. This update aims to ensure any new keybinding commands are incorporated into the user's development environment upon installation, improving the efficiency and ease of use.

--- install.ps1
+++ install.ps1
@@ -1,5 +1,10 @@
 Invoke-RestMethod https://raw.githubusercontent.com/chubbyhippo/code-settings/main/install-extensions.sh | Invoke-Expression
 
+# settings.json
 $url = "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json"
 $destinationPath = "$Home\AppData\Roaming\Code\User\settings.json"
+Invoke-WebRequest -Uri $url -OutFile $destinationPath
+# keybindings.json
+$url = "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/keybindings.json"
+$destinationPath = "$Home\AppData\Roaming\Code\User\keybindings.json"
 Invoke-WebRequest -Uri $url -OutFile $destinationPath
