d02589ca2727d6427a70714f9ae05c3e0978209c
ch <mkmsacc@outlook.com>
2024-03-03 07:35:13 +0700

    Remove PowerShell and Java extensions installation
    This commit removes the installation of PowerShell and Java extensions from the install-extensions.sh script. This is possibly due to changes in the required development environment or refocusing on other languages.

--- install-extensions.sh
+++ install-extensions.sh
@@ -4,12 +4,8 @@
 code --install-extension vscodevim.vim
 # git
 code --install-extension eamodio.gitlens
-# powershell
-code --install-extension ms-vscode.powershell
 # static code analysis
 code --install-extension sonarsource.sonarlint-vscode
-# java
-code --install-extension vscjava.vscode-java-pack
 # javascript
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension xabikos.JavaScriptSnippets
