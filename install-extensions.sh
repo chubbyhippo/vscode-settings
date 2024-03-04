6c51ab301095922ff59106f9b39d27f308217526
ch <mkmsacc@outlook.com>
2024-03-04 17:52:16 +0700

    The commit message provided does not match the code diff. Here is a correct version:
    Add Java-related extensions to install script
    
    Java-related extensions vscode-java-pack, vscode-gradle, and vscode-lombok have been added to install-extensions.sh. These additions include tools for Java language support, build automation with Gradle, and support for Lombok.

--- install-extensions.sh
+++ install-extensions.sh
@@ -6,6 +6,10 @@ code --install-extension vscodevim.vim
 code --install-extension eamodio.gitlens
 # static code analysis
 code --install-extension sonarsource.sonarlint-vscode
+# java
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-gradle
+code --install-extension vscjava.vscode-lombok
 # javascript
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension xabikos.JavaScriptSnippets
