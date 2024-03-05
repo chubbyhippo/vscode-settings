d5e657c6488ea91ad75583a3d8ed49376d03ba24
ch <mkmsacc@outlook.com>
2024-03-05 15:05:09 +0700

    Refactor Invoke-WebRequest calls in install.ps1
    Simplified the Invoke-WebRequest calls in install.ps1 file by eliminating the use of variables for url and destinationPath. Directly embedded the values in the function call for cleaner and more efficient code.

--- install.ps1
+++ install.ps1
@@ -1,10 +1,6 @@
 # install extensions
 Invoke-RestMethod https://raw.githubusercontent.com/chubbyhippo/code-settings/main/install-extensions.sh | Invoke-Expression
 # settings.json
-$url = "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json"
-$destinationPath = "$Home\AppData\Roaming\Code\User\settings.json"
-Invoke-WebRequest -Uri $url -OutFile $destinationPath
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json" -OutFile "$Home\AppData\Roaming\Code\User\settings.json"
 # keybindings.json
-$url = "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/keybindings.json"
-$destinationPath = "$Home\AppData\Roaming\Code\User\keybindings.json"
-Invoke-WebRequest -Uri $url -OutFile $destinationPath
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/keybindings.json" -OutFile "$Home\AppData\Roaming\Code\User\keybindings.json"
