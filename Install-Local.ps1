9c19f5a75cac32350ab28dc1a9d98cc7bab77138
chubbyhippo <mkmsacc@outlook.com>
2024-04-08 15:18:37 +0700

    Force ctrl+c/v to vim bindings

---
+++ Install-Local.ps1
@@ -0,0 +1,4 @@
+# settings.json
+Copy-Item -Path "settings.json" -Destination "$Home\AppData\Roaming\Code\User\settings.json" -Force
+# keybindings.json
+Copy-Item -Path "keybindings.json" -Destination "$Home\AppData\Roaming\Code\User\keybindings.json" -Force
