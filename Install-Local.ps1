3be32e5a5f8c6cb8fb69f5ca6f44c17823e8ab39
chubbyhippo <mkmsacc@outlook.com>
2024-04-08 16:02:38 +0700

    Add install extensions line

--- Install-Local.ps1
+++ Install-Local.ps1
@@ -2,3 +2,5 @@
 Copy-Item -Path "settings.json" -Destination "$Home\AppData\Roaming\Code\User\settings.json" -Force
 # keybindings.json
 Copy-Item -Path "keybindings.json" -Destination "$Home\AppData\Roaming\Code\User\keybindings.json" -Force
+# extensions
+bash .\install-extensions.sh
