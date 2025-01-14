99fc9ab57b716e9553600381e8317974e8108e7b
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-14 17:33:36 +0700

    Update install-extensions.sh

--- install-extensions.sh
+++ install-extensions.sh
@@ -4,29 +4,3 @@
 code --force --install-extension vscodevim.vim
 # git
 code --force --install-extension eamodio.gitlens
-# static code analysis
-code --force --install-extension sonarsource.sonarlint-vscode
-code --force --install-extension redhat.fabric8-analytics
-# docker
-code --force --install-extension ms-azuretools.vscode-docker
-# java
-code --force --install-extension vscjava.vscode-java-pack
-code --force --install-extension vscjava.vscode-gradle
-code --force --install-extension vscjava.vscode-lombok
-# quarkus
-code --force --install-extension redhat.vscode-quarkus
-# spring
-code --force --install-extension vmware.vscode-boot-dev-pack
-# javascript
-code --force --install-extension dbaeumer.vscode-eslint
-code --force --install-extension xabikos.JavaScriptSnippets
-code --force --install-extension christian-kohler.npm-intellisense
-code --force --install-extension christian-kohler.path-intellisense
-# react
-code --force --install-extension dsznajder.es7-react-js-snippets
-# golang
-code --force --install-extension golang.go
-# rust
-code --force --install-extension rust-lang.rust-analyzer
-# powershell
-code --force --install-extension ms-vscode.powershell
