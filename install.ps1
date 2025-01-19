3e942959c62f9777c97943c627e1a71969b28953
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-19 08:32:17 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 # settings.json
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main/settings.json" -OutFile "$Home\AppData\Roaming\Code\User\settings.json"
 # keybindings.json
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/keybindings.json" -OutFile "$Home\AppData\Roaming\Code\User\keybindings.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main/keybindings.json" -OutFile "$Home\AppData\Roaming\Code\User\keybindings.json"
