a5a0ef015274f1ca5ced7fe30a979e03214d39b7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-19 08:27:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,9 +23,9 @@ esac
 # install settings.json
 echo "$settings_json_destination"
 if [ "$(command -v curl)" ]; then
-    curl https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json -o "$settings_json_destination"
+    curl https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main/settings.json -o "$settings_json_destination"
     curl https://raw.githubusercontent.com/chubbyhippo/code-settings/main/keybindings.json -o "$keybindings_json_destination"
 elif [ "$(command -v wget)" ]; then
-    wget https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json -P "$settings_json_destination"
+    wget https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main/settings.json -P "$settings_json_destination"
     wget https://raw.githubusercontent.com/chubbyhippo/code-settings/main/keybindings.json -P "$keybindings_json_destination"
 fi
