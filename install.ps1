da2d52637ec9653fecac5cd98d048d3cff73f30c
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-19 08:26:11 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,3 @@
-# install extensions
-Invoke-RestMethod https://raw.githubusercontent.com/chubbyhippo/code-settings/main/install-extensions.sh | Invoke-Expression
 # settings.json
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chubbyhippo/code-settings/main/settings.json" -OutFile "$Home\AppData\Roaming\Code\User\settings.json"
 # keybindings.json
