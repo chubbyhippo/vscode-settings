2c7f81357926ce2a76c013fab8871ef78c942fc8
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-20 13:45:15 +0700

    Create install-extension-js.sh

---
+++ install-extension-js.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# keybindings
+code --force --install-extension vscodevim.vim
+# git
+code --force --install-extension eamodio.gitlens
+# numbered bookmark
+code --force --install-extension alefragnani.numbered-bookmarks
