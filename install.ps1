c963560e1c9d9a9c78cd4561804a99107002c2f1
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-20 09:13:08 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,3 +2,5 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main/settings.json" -OutFile "$Home\AppData\Roaming\Code\User\settings.json"
 # keybindings.json
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main/keybindings.json" -OutFile "$Home\AppData\Roaming\Code\User\keybindings.json"
+# install basic extensions
+irm https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main/install-extensions.ps1 | iex
