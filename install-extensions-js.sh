a657d3ae3a145dd5ea7621aba82e616966c4b960
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-20 14:40:04 +0700

    Update install-extensions-js.sh

--- install-extensions-js.sh
+++ install-extensions-js.sh
@@ -6,6 +6,10 @@ code --force --install-extension vscodevim.vim
 code --force --install-extension eamodio.gitlens
 # numbered bookmark
 code --force --install-extension alefragnani.numbered-bookmarks
+# docker
+code --force --install-extension ms-azuretools.vscode-docker
+# yaml
+code --force --install-extension redhat.vscode-yaml
 # prettier
 code --force --install-extension esbenp.prettier-vscode
 # linter
