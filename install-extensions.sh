446b0dda12eb98f4318d3c72a5d84e6127773af5
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-20 14:09:01 +0700

    Update install-extensions.sh

--- install-extensions.sh
+++ install-extensions.sh
@@ -6,5 +6,7 @@ code --force --install-extension vscodevim.vim
 code --force --install-extension eamodio.gitlens
 # numbered bookmark
 code --force --install-extension alefragnani.numbered-bookmarks
+# docker
+code --force --install-extension ms-azuretools.vscode-docker
 # yaml
 code --force --install-extension redhat.vscode-yaml
