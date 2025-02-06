7827b68c86994b77c7018565e070b68a122295d7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-02-06 20:56:56 +0700

    Delete Install-Local.ps1

--- Install-Local.ps1
+++
@@ -1,6 +0,0 @@
-# settings.json
-Copy-Item -Path "settings.json" -Destination "$Home\AppData\Roaming\Code\User\settings.json" -Force
-# keybindings.json
-Copy-Item -Path "keybindings.json" -Destination "$Home\AppData\Roaming\Code\User\keybindings.json" -Force
-# extensions
-bash .\install-extensions.sh
