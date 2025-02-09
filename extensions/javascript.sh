05d188649dacebab0014cd1cb3c31646d6a2ebac
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-02-09 20:04:46 +0700

    Create javascript.sh

---
+++ extensions/javascript.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# prettier
+code --force --install-extension esbenp.prettier-vscode
+# linter
+code --force --install-extension dbaeumer.vscode-eslint
+# debugger
+code --force --install-extension ms-vscode.js-debug
+code --force --install-extension ms-vscode.js-debug-companion
+# vitest
+code --force --install-extension vitest.explorer
