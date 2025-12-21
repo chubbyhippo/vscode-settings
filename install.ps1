02f1bb85c7604f58a9fe871254ee2ffa74c335a0
chubbyhippo <mkmsacc@outlook.com>
2025-12-21 21:14:58 +0700

    Standardize variable naming in install script

--- install.ps1
+++ install.ps1
@@ -1,8 +1,8 @@
-$RAW_SETTINGS_URL = "https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main"
+$RawSettingsUrl = "https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main"
 
 # settings.json
-Invoke-WebRequest -Uri "$RAW_SETTINGS_URL/settings.json" -OutFile "$Home\AppData\Roaming\Code\User\settings.json"
+Invoke-WebRequest -Uri "$RawSettingsUrl/settings.json" -OutFile "$Home\AppData\Roaming\Code\User\settings.json"
 # keybindings.json
-Invoke-WebRequest -Uri "$RAW_SETTINGS_URL/keybindings.json" -OutFile "$Home\AppData\Roaming\Code\User\keybindings.json"
+Invoke-WebRequest -Uri "$RawSettingsUrl/keybindings.json" -OutFile "$Home\AppData\Roaming\Code\User\keybindings.json"
 # install basic extensions
-Invoke-RestMethod "$RAW_SETTINGS_URL/install-extensions.sh" | Invoke-Expression
+Invoke-RestMethod "$RawSettingsUrl/install-extensions.sh" | Invoke-Expression
