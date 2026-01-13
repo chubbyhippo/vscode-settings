d8855da2d8a0fd5dd7668ff8d19eee9129f9bfdd
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-01-13 08:07:59 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,4 +5,4 @@ Invoke-WebRequest -Uri "$RawSettingsUrl/settings.json" -OutFile "$Home\AppData\R
 # keybindings.json
 Invoke-WebRequest -Uri "$RawSettingsUrl/keybindings.json" -OutFile "$Home\AppData\Roaming\Code\User\keybindings.json"
 # install basic extensions
-Invoke-RestMethod "$RawSettingsUrl/install-extensions.sh" | Invoke-Expression
+Invoke-RestMethod "$RawSettingsUrl/install-default-extensions.sh" | Invoke-Expression
