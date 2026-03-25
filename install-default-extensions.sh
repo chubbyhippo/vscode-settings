b0cddfa5fead418b2956ec062eadfc8680e611bf
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-03-25 08:43:37 +0700

    Update install-default-extensions.sh

--- install-default-extensions.sh
+++ install-default-extensions.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env sh
 
 # keybindings
-code --force --install-extension vscodevim.vim
+code --force --install-extension vscodevim.vim --ignore-certificate-errors
 # github theme
-code --force --install-extension GitHub.github-vscode-theme
+code --force --install-extension GitHub.github-vscode-theme --ignore-certificate-errors
 # material icon theme
-code --force --install-extension PKief.material-icon-theme
+code --force --install-extension PKief.material-icon-theme --ignore-certificate-errors
 # sonarqube
-code --force --install-extension SonarSource.sonarlint-vscode
+code --force --install-extension SonarSource.sonarlint-vscode --ignore-certificate-errors
 # git
-code --force --install-extension eamodio.gitlens
+code --force --install-extension eamodio.gitlens --ignore-certificate-errors
 # numbered bookmark
-code --force --install-extension alefragnani.numbered-bookmarks
+code --force --install-extension alefragnani.numbered-bookmarks --ignore-certificate-errors
 # docker
-code --force --install-extension ms-azuretools.vscode-docker
+code --force --install-extension ms-azuretools.vscode-docker --ignore-certificate-errors
 # wsl
-code --force --install-extension ms-vscode-remote.remote-wsl
+code --force --install-extension ms-vscode-remote.remote-wsl --ignore-certificate-errors
 # prettier
-code --force --install-extension esbenp.prettier-vscode
+code --force --install-extension esbenp.prettier-vscode --ignore-certificate-errors
 # yaml
-code --force --install-extension redhat.vscode-yaml
+code --force --install-extension redhat.vscode-yaml --ignore-certificate-errors
 # xml
-code --force --install-extension redhat.vscode-xml
+code --force --install-extension redhat.vscode-xml --ignore-certificate-errors
 # ]m [m
-code --force --install-extension mishkinf.goto-next-previous-member
+code --force --install-extension mishkinf.goto-next-previous-member --ignore-certificate-errors
 # rest client
-code --force --install-extension humao.rest-client
+code --force --install-extension humao.rest-client --ignore-certificate-errors
 # asciidoctor
-code --force --install-extension asciidoctor.asciidoctor-vscode
+code --force --install-extension asciidoctor.asciidoctor-vscode --ignore-certificate-errors
 # markdown execute
-code --force --install-extension hanskre.markdown-execute
+code --force --install-extension hanskre.markdown-execute --ignore-certificate-errors
