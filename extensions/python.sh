728535ce72ff262c882fda6222b62d8fbcd854f5
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-03-25 08:44:17 +0700

    Update python.sh

--- extensions/python.sh
+++ extensions/python.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-code --force --install-extension ms-python.python
-code --force --install-extension ms-python.debugpy
-code --force --install-extension ms-toolsai.jupyter
+code --force --install-extension ms-python.python --ignore-certificate-errors
+code --force --install-extension ms-python.debugpy --ignore-certificate-errors
+code --force --install-extension ms-toolsai.jupyter --ignore-certificate-errors
