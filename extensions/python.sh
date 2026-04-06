8fd87b7ef14c653e3ae7706c608de561ac573522
chubbyhippo <mkmsacc@outlook.com>
2026-04-06 14:19:16 +0700

    Install `ms-python.black-formatter` extension in python.sh

--- extensions/python.sh
+++ extensions/python.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 code --force --install-extension ms-python.python
+code --force --install-extension ms-python.black-formatter
 code --force --install-extension ms-python.debugpy
 code --force --install-extension ms-toolsai.jupyter
