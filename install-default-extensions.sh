df5557302bd1ea653ca8ae8385eee6c2a00708c3
chubbyhippo <mkmsacc@outlook.com>
2026-04-24 10:04:56 +0700

    Remove installation of unused markdown-execute extension.

--- install-default-extensions.sh
+++ install-default-extensions.sh
@@ -29,7 +29,5 @@ code --force --install-extension mishkinf.goto-next-previous-member
 code --force --install-extension humao.rest-client
 # asciidoctor
 code --force --install-extension asciidoctor.asciidoctor-vscode
-# markdown execute
-code --force --install-extension hanskre.markdown-execute
 # mise
 code --force --install-extension hverlin.mise-vscode
