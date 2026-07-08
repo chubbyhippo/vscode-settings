798b5342426213b517297b1b03ea42ec989792c6
chubbyhippo <mkmsacc@outlook.com>
2026-07-08 08:23:52 +0700

    Remove `keybindings.json` references from VS Code installation script for simplified configuration setup.

--- install.sh
+++ install.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env sh
 
-# Initialize destination variables for settings.json and keybindings.json
+# Initialize destination variable for settings.json
 settings_json_destination=""
-keybindings_json_destination=""
 
 # Detect the operating system and set file paths accordingly
 case $(uname) in
 "Linux")
   # For Linux systems
   settings_json_destination="$HOME/.config/Code/User/settings.json"
-  keybindings_json_destination="$HOME/.config/Code/User/keybindings.json"
   ;;
 "Darwin")
   # For macOS systems
   settings_json_destination="$HOME/Library/Application Support/Code/User/settings.json"
-  keybindings_json_destination="$HOME/Library/Application Support/Code/User/keybindings.json"
   ;;
 "MINGW"*)
   # For Windows systems using MinGW
@@ -23,12 +20,10 @@ case $(uname) in
     # If VSCode was installed via Scoop
     echo "vscode was installed via Scoop."
     settings_json_destination="$HOME/scoop/apps/vscode/current/data/user-data/User/settings.json"
-    keybindings_json_destination="$HOME/scoop/apps/vscode/current/data/user-data/User/keybindings.json"
     ;;
   *)
     # Default VSCode installation path in Windows
     settings_json_destination="$HOME/AppData/Roaming/Code/User/settings.json"
-    keybindings_json_destination="$HOME/AppData/Roaming/Code/User/keybindings.json"
     ;;
   esac
   ;;
@@ -40,19 +35,16 @@ esac
 
 # Print the destination paths for verification
 echo "$settings_json_destination"
-echo "$keybindings_json_destination"
 
 RAW_SETTINGS_URL="https://raw.githubusercontent.com/chubbyhippo/vscode-settings/refs/heads/main"
 
-# Download settings.json and keybindings.json using curl or wget
+# Download settings.json using curl or wget
 if [ "$(command -v curl)" ]; then
   # Use curl if available
   curl -k "$RAW_SETTINGS_URL/settings.json" -o "$settings_json_destination"
-  curl -k "$RAW_SETTINGS_URL/keybindings.json" -o "$keybindings_json_destination"
 elif [ "$(command -v wget)" ]; then
   # Use wget if available
   wget "$RAW_SETTINGS_URL/settings.json" -P "$settings_json_destination"
-  wget "$RAW_SETTINGS_URL/keybindings.json" -P "$keybindings_json_destination"
 fi
 
 # Install basic VSCode extensions by executing a remote script
